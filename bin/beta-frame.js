f400e0cb6f184e17919d0a3659ae29426a2c4073
李志礼 <931035186@qq.com>
2018-07-03 23:45:45 +0800

    feat:add-create-project

--- bin/beta-frame.js
+++ bin/beta-frame.js
@@ -1,1 +1,18 @@
-console.log(JSON.stringify(process.argv));
+#!/usr/bin/env node
+const path = require('path');
+const projectName = process.argv[0];
+let src = path.resolve(__dirname, projectName, 'src');
+console.log(src);return;
+const cp = require('child_process');
+const cmd = `create-react-app ${projectName}`;
+let pro = cp.execSync(cmd, function (err, stdout, stderr) {
+    if(!err) {
+        console.log(stdout);
+    } else {
+        console.log(err);
+    }
+});
+
+pro.on('data', function (data) {
+    console.log(data);
+})
